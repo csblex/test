7a4de259d322998211b4ea74fb35619d13b57a64
git-arbitrarysystems <git@arbitrarysystems.com>
2012-10-08 21:43:03 +0200

    canvastest

--- www/js/inject.js
+++ www/js/inject.js
@@ -1,3 +1,62 @@
+// Wait for PhoneGap to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// PhoneGap is ready
+//
+function onDeviceReady() {
+    initCanvas();
+}
+
+// the canvas context
+var context;
+var lineY = 0;
+var w;
+var h;
+var iv;
+
+function initCanvas(){
+    w = $('#canvaspage').width();
+    h = $('#canvaspage').height()-3;
+    $('#canvaspage').html('<canvas id="cv" width="'+w+'" height="'+h+'"></canvas>');
+    var canvas = document.getElementById("cv");
+    context = canvas.getContext("2d");
+    
+    console.log('hello canvas',w,h,context);
+    iv = setInterval( addLine , 20 );
+
+
+}
+
+function addLine(){
+    var rex = rhex();
+    context.strokeStyle = rex;
+    context.beginPath();
+    context.moveTo(0,lineY);
+    context.lineTo(w,lineY);
+    context.stroke();
+    lineY = (lineY+1) % h;
+    //console.log(lineY , rex );
+}
+
+function rhex(){
+    var pool = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    var hex = '#';
+    for( var i = 0 ; i < 6 ; i++){
+        hex += pool[ Math.floor( Math.random() * pool.length ) ];
+    }
+    return hex;
+}
+
+
+
+
+
+
+/*
+
+ACCELEROMETER EXAMPLE
+
 // The watch id references the current `watchAcceleration`
 var watchID = null;
 
@@ -52,3 +111,5 @@ function onError() {
     var element = document.getElementById('accelerometer');
     element.innerHTML = 'Error!';
 }
+
+*/
